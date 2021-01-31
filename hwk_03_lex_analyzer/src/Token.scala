99646850d4bd8c1bc7bccb187f222803a4f3e940
Thyago Mota <tmota@msudenver.edu>
2021-01-31 14:38:11 -0700

    updates

---
+++ hwk_03_lex_analyzer/src/Token.scala
@@ -0,0 +1,12 @@
+/*
+ * CS3210 - Principles of Programming Languages - Spring 2021
+ * Instructor: Thyago Mota
+ * Description: Homework 03  - Token
+ * Student(s) Name(s):
+ */
+
+// TODO: finish this part of the code
+object Token extends Enumeration {
+  val EOF             = Value
+  val DECLARE         = Token.DECLARE
+}
